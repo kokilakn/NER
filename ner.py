9d97ecbcb75a09b4d05b8c613ac8eb2b27f9a0ce
Kokila K N <kokilakn13@gmail.com>
2021-09-06 20:32:58 +0530

    Update ner.py

--- ner.py
+++ ner.py
@@ -7,27 +7,27 @@ from google_trans_new import google_translator
 nlp = spacy.load('en_core_web_sm')
 
 def normalization(sentence,tokenize=True):
-	textBlb = TextBlob(sentence)
-	textCorrected = textBlb.correct()
-    sentence = html.unescape(sentence)
-    regex = re.compile(r'[\n\r\t]')
-    sentence = regex.sub(" ", sentence)
-    sentence = re.sub(r'http\S+', '', sentence)
-    sentence = re.sub('@[^\s]+','',sentence)
-    try:
-        lang = detect(sentence)
-        if lang != 'en':
-            translator = google_translator()
-            sentence = translator.translate (sentence,lang_tgt='en')
-    except :
-        try:
-            translator = google_translator()
-            sentence = translator.translate (sentence,lang_tgt='en')
-        except:
-            pass
-    sentence = contractions.fix(sentence)
-    sentence = unidecode.unidecode(sentence)
-    return sentence
+   textBlb = TextBlob(sentence)
+   textCorrected = textBlb.correct()
+   sentence = html.unescape(sentence)
+   regex = re.compile(r'[\n\r\t]')
+   sentence = regex.sub(" ", sentence)
+   sentence = re.sub(r'http\S+', '', sentence)
+   sentence = re.sub('@[^\s]+','',sentence)
+   try:
+          lang = detect(sentence)
+          if lang != 'en':
+              translator = google_translator()
+              sentence = translator.translate (sentence,lang_tgt='en')
+   except :
+          try:
+              translator = google_translator()
+              sentence = translator.translate (sentence,lang_tgt='en')
+          except:
+              pass
+   sentence = contractions.fix(sentence)
+   sentence = unidecode.unidecode(sentence)
+   return sentence
 
 def tags(sentence):
 	sentence=normalization(sentence)
